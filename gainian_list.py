895a8804c9ba25a3793fe48707bf90c7d14bcf8d
qianzhiqin <Betterman@37>
2019-08-09 23:00:33 +0800

    init

--- gainian_list.py
+++ gainian_list.py
@@ -2,14 +2,31 @@ import os
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import ua_utils
 
+ua = '''Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3
+Accept-Encoding: gzip, deflate
+Accept-Language: zh-CN,zh;q=0.9
+Cache-Control: max-age=0
+Connection: keep-alive
+Cookie: __utma=156575163.1952850795.1561647809.1561647809.1561647809.1; __utmz=156575163.1561647809.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1561647779,1561647809,1561647953; v=AjaaGTngd6AWVQPEOvIEfR_Oh2c7V3qRzJuu9aAfIpm049iRCOfKoZwr_glz
+DNT: 1
+Host: q.10jqka.com.cn
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'''
+
+headers = ua_utils.get_headers(ua)
 path = os.getcwd() + '/data/gainian.csv'
 data = pd.read_csv(path, sep=',')
 url = 'http://q.10jqka.com.cn/gn/detail/field/264648/order/desc/page/1/ajax/1/code/'
 # print(data)
 for index, row in data.iterrows():
-	code = row['code']
-	neme = row['name']
-	url = url + str(code)
-	response = requests.get(url)
-	print(response)
+    code = row['code']
+    neme = row['name']
+    url = url + str(code)
+    response = requests.get(url, headers=headers).text
+    soup = BeautifulSoup(response, 'lxml')
+    list = soup.select('a[target="_blank"]')
+    print(url)
+    page = soup.select_one('span[class="page_info"]')
+    print(page)
--- souhu.py
+++ souhu.py
@@ -1,17 +1,35 @@
 from selenium import webdriver
 
+import time
+
+from selenium.common.exceptions import StaleElementReferenceException
+
 qufu_default = '纵横双线'
 browser = webdriver.Chrome()
 for i in range(1, 500):
-	browser.get(
-		'http://tl.cyg.changyou.com/goods/selling?world_id=0&price=1-1000&have_chosen=price*1-1000&page_num=' + str(
-			i) + '#goodsTag')
-	server = browser.find_elements_by_class_name('server-info')
-	# print('第' + str(i) + '页码')
-	for aa in server:
-		qufu = aa.text
-		title = aa.find_element_by_xpath('../../dt').text
-		detail = aa.find_element_by_xpath('../../dd[1]').text
-		price = aa.find_element_by_xpath('../../../div').text
-		if qufu_default in qufu:
-			print(price + '   ' + title + '  ' + detail + '  ' + aa.text)
+    page = 'http://tl.cyg.changyou.com/goods/selling?world_id=0&price=1-300&have_chosen=price*1-1000&page_num=' + str(
+        i) + '#goodsTag'
+    browser.get(page)
+    server = browser.find_elements_by_class_name('server-info')
+    # print('第' + str(i) + '页码')
+
+    for i in range(0, len(server)):
+        aa = server[i]
+        try:
+            qufu = aa.text
+            title = aa.find_element_by_xpath('../../dt').text
+            url = aa.find_element_by_xpath('../../dt/a').get_attribute('href')
+            detail = aa.find_element_by_xpath('../../dd[1]').text
+            price = aa.find_element_by_xpath('../../../div').text
+            if qufu_default in qufu:
+                browser.get(url)
+                # row2 = browser.find_elements_by_class_name('row2')
+                row2 = browser.find_element_by_xpath('//*[@id="goods-detail"]/div/div[4]/div[1]/div/div[6]/span').text
+                # browser.back()
+                # time.sleep(2)
+                browser.get(page)
+                if int(row2) >= 250:
+                    print(price + '  ' + row2 + '   ' + title + '  ' + detail + '  ' + ' ' + qufu + '  ' + str(url))
+        except StaleElementReferenceException as e:
+            server = browser.find_elements_by_class_name('server-info')
+            pass
