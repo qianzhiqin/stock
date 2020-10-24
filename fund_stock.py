57ba8dc717a742195f685eb3efa229aa8bf35fef
qianzhiqin@126.com <qzq84607952>
2020-10-24 22:31:54 +0800

    基金

--- fund_stock.py
+++ fund_stock.py
@@ -9,13 +9,13 @@ path = os.getcwd() + '/data/fund.csv'
 data = pd.read_csv(path, sep=',', dtype={'symbol': str})
 file_name = os.getcwd() + '/data/chicang.csv'
 
-with open(file_name, 'w',encoding="utf-8") as file_obj:
+with open(file_name, 'w', encoding="utf-8") as file_obj:
 	for index, row in data.iterrows():
 		print(index)
 		succ6 = row[10]
 		symbol = row[0]
 		name = row[1]
-		if succ6 > 50:
+		if succ6 > 25:
 			result = requests.get("http://fund.eastmoney.com/" + symbol + ".html", verify=False)
 			result.encoding = result.apparent_encoding
 			res = result.text[3:]
@@ -29,9 +29,11 @@ with open(file_name, 'w',encoding="utf-8") as file_obj:
 						if "股票名称" not in tmp and "债券名称" not in tmp:
 							val = tmp.split(' ')
 							if len(val) == 6:
-								stock = symbol + '\t' + name + '\t' + val[0] + '\t' + val[2].replace('%','')
+								stock = symbol + '\t' + name + '\t' + str(succ6) + '\t' + val[0] + '\t' + val[2].replace('%', '')
 								file_obj.write(stock + "\n")
+								print(stock)
 							elif len(val) == 5:
-								stock = symbol + '\t' + name + '\t' + val[0] + '\t' + val[3].replace('%','')
+								stock = symbol + '\t' + name + '\t' + str(succ6) + '\t' + val[0] + '\t' + val[3].replace('%', '')
 								file_obj.write(stock + "\n")
+								print(stock)
 		file_obj.flush()
